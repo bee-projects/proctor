830c10abdc1f1f3fd3673f22d8cd373da2caa765
Ameer Deen <ameer.deen@cba.com.au>
2019-08-13 10:03:50 +1000

    Fixing package names

--- main.go
+++ main.go
@@ -1,1 +1,10 @@
-package proctor
+package main
+
+import (
+	"fmt"
+	"github.com/bee-projects/proctor/plugins/authenticator"
+)
+
+func main() {
+	fmt.Println(authenticator.HandshakeConfig.MagicCookieKey)
+}
